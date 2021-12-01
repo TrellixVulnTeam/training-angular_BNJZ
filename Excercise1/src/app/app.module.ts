a655b4d6f2119090929c586c77f6780fe513124e
ThienNN11 <nhothien110198@gmail.com>
2021-12-01 12:31:57 +0700

    update

--- Excercise1/src/app/app.module.ts
+++ Excercise1/src/app/app.module.ts
@@ -1,55 +1,41 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AdminComponent } from './components/login/admin/admin.component';
 import { UserComponent } from './components/login/user/user.component';
 import { CreateComponent } from './components/login/create/create.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AdminProfileComponent } from './components/login/admin-profile/admin-profile.component';
+import { AdminProfileComponent } from './components/login/admin/components/admin-profile/admin-profile.component';
+import { UserManagementComponent } from './components/login/admin/components/user-management/user-management.component';
+import { EditAccountComponent } from './components/login/admin/components/user-management/components/edit-account/edit-account.component';
+import { UserProfileComponent } from './components/login/user/components/user-profile/user-profile.component';
 
-const appRoute : Routes = [
-  {
-    path:'',
-    redirectTo: '/login',
-    pathMatch:'full'
-  },
-  {
-    path: "login",
-    component: LoginComponent
-  },{
-    path:"admin",
-    component: AdminComponent
-    
-  },{
-    path:"user",
-    component: UserComponent
-  },{
-    path:"create",
-    component: CreateComponent
-  }
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CreateComponent,
-    AdminProfileComponent
+    AdminProfileComponent,
+    UserManagementComponent,
+    AdminComponent,
+    UserComponent,
+    EditAccountComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoute)
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
